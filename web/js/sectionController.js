bfe1497ba77a1206da4a51f5d7ab37cadcbe9344
Borja <borja.mz8@gmail.com>
2015-03-03 19:41:29 +0100

    Corregidos fallos y angularjs
    1. ya se puede usar angularjs (si se quiere, yo lo usaré)
    2. corregidos fallos que salían en la cosola de archivos no encontrados,
    estaban mal las rutas de algunos js
    3. En el navabar si le das al loogo o al nombre de la web si no estas
    registrado te lleva a inicio si lo está te lleva al panel del tutor
    4. validacion del formulario de registro, no te deja seguir si no metes
    los campos requeridos o si metes los mails mal

---
+++ web/js/sectionController.js
@@ -0,0 +1,9 @@
+var app = angular.module('home', []);
+
+app.controller('sectionController', ['$scope', function($scope) {
+	newTutor = {
+		pass : '',
+		repeatedPass : '',
+		validated : false
+	}
+}]);
