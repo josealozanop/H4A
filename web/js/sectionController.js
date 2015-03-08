790ef8c6b46eca1bbbf706ad11c60d7c0fbcbf6b
Borja <borja.mz8@gmail.com>
2015-03-08 12:45:05 +0100

    Segunda versión del navbar
    Ya está activo el navMenu del navBar

--- web/js/sectionController.js
+++ web/js/sectionController.js
@@ -1,12 +1,28 @@
 var app = angular.module('home', []);
 
 app.controller('sectionController', ['$scope', function($scope) {
-	navBarMobile = {
+	$scope.navBarMobile = {
+		
 		expand_login : false,
-		error : false,
-		click : function () {
+		error : false, //error a la hora de hacer login
+		expand_menu : false,
+		
+		click_login : function () {
 			if(!this.expand_login) {
 				this.expand_login = true
+			}
+			else {
+				this.expand_login = false
+			}
+		},
+		
+		click_menu : function () {
+			console.log("hola");
+			if(!this.expand_menu) {
+				this.expand_menu = true
+			}
+			else {
+				this.expand_menu = false
 			}
 		}
 	}
