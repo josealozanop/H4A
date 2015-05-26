51c020793cb202ce9dc174731f5621e3e6be8101
josealozanop <josealozanop@gmail.com>
2015-05-26 18:02:30 +0200

    Configuracion de colores

--- web/js/configUser.js
+++ web/js/configUser.js
@@ -1,10 +1,17 @@
 app.controller('configUser', ['$scope', 'asyncServices', '$attrs','$filter', function($scope,asyncServices,$attrs,$filter) {
-	
+	var ColPrin;
+	var ColSec;
 	$scope.init = function(){
+		ColPrin="";
+		ColSec="";
+		Gama1 = false;
+		Gama2 = false;
+		DifCrom = false;
 		$scope.idUsuario = $attrs.idUsuario;
 		var devicesData = angular.fromJson($attrs.devicesData);
 		var devicesNames = angular.fromJson(window.atob($attrs.names));
 		$scope.devices = [];
+		console.log(DifCrom);
 		for(i in devicesData){
 			var newDevice = angular.fromJson(devicesData[i])[0];
 			newDevice.name = devicesNames[i];
@@ -33,7 +40,15 @@ app.controller('configUser', ['$scope', 'asyncServices', '$attrs','$filter', fun
 		var allowed = $scope.devices[$scope.selectedDevice].config.layout.verticalAllowed;
 		return allowed;
 	}
-	
+	$scope.isDifCrom = function(){
+		return DifCrom;		
+	}
+	$scope.isGama2 = function(){
+		return Gama2;		
+	}
+	$scope.isGama1 = function(){
+		return Gama1;		
+	}
 	$scope.range = be_range;
 	
 	$scope.setVerticalAllowed = function(){
@@ -46,11 +61,88 @@ app.controller('configUser', ['$scope', 'asyncServices', '$attrs','$filter', fun
 			$scope.devices[$scope.selectedDevice].config.layout.verticalAllowed = true;
 		}
 	}
-	
+	$scope.setDifCrom = function(){
+		console.log(DifCrom)
+		if(DifCrom){
+			DifCrom = false;			
+			ColPrin="";
+			ColSec="";
+		}
+		else{
+			DifCrom = true;
+			ColPrin="blue";
+			ColSec="orange";
+		}
+	}
 	$scope.setSelectedDevice = function(index){
 		$scope.selectedDevice = index;
 	}
-	
+	$scope.btnPricolor = function(index){
+		$scope.CPRed= "btn btn-danger";
+		$scope.CPOr= "btn btn-warning";		
+		$scope.CPYe= "btn btn-default";
+		$scope.CPBlu= "btn btn-primary";
+		$scope.CPPur= "btn btn-info";
+		$scope.CSRed= "btn btn-danger";
+		$scope.CSOr= "btn btn-warning";		
+		$scope.CSYe= "btn btn-default";
+		$scope.CSBlu= "btn btn-primary";
+		$scope.CSPur= "btn btn-info";
+		switch (index){
+			case "red":
+				$scope.CPRed= "btn btn-danger btn-lg active";
+				Gama2=true;
+				Gama1=false;
+			break;
+			case "orange":
+				$scope.CPOr= "btn btn-warning btn-lg active";
+				Gama2=true;
+				Gama1=false;
+			break;
+			case "yellow":
+				$scope.CPYe= "btn btn-default btn-lg active";
+				Gama2=true;
+				Gama1=false;
+			break;
+			case "blue":
+				$scope.CPBlu= "btn btn-primary btn-lg active";
+				Gama1=true;
+				Gama2=false;
+			break;
+			case "purple":
+				$scope.CPPur= "btn btn-info btn-lg active";
+				Gama1=true;
+				Gama2=false;
+			break;
+			}
+		ColPrin=index;
+		ColSec="";
+	}
+	$scope.btnSecolor = function(index){
+		$scope.CSRed= "btn btn-danger";
+		$scope.CSOr= "btn btn-warning";		
+		$scope.CSYe= "btn btn-default";
+		$scope.CSBlu= "btn btn-primary";
+		$scope.CSPur= "btn btn-info";
+		switch (index){
+			case "red":
+				$scope.CSRed= "btn btn-danger btn-lg active";
+			break;
+			case "orange":
+				$scope.CSOr= "btn btn-warning btn-lg active";
+			break;
+			case "yellow":
+				$scope.CSYe= "btn btn-default btn-lg active";
+			break;
+			case "blue":
+				$scope.CSBlu= "btn btn-primary btn-lg active";
+			break;
+			case "purple":
+				$scope.CSPur= "btn btn-info btn-lg active";
+			break;
+			}
+		ColSec=index;
+	}
 	$scope.sinfo = function(){
 		console.log($scope)
 	}
