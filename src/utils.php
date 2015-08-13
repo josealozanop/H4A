8b5fb1c039f0a47f95e8395225ed3e82672c0cb0
Borja <borja.mz8@gmail.com>
2015-08-13 17:45:22 +0200

    raspberry bugfix

--- src/utils.php
+++ src/utils.php
@@ -65,3 +65,46 @@ function getMAC(){
 	}
 	return $mac;
 }
+
+
+
+function obtenerMAC() {
+                $ipAddress=$_SERVER['REMOTE_ADDR'];
+                        $comando=false;
+                        if(($ipAddress==$_SERVER['SERVER_ADDR'] || $ipAddress=='127.0.0.1' || $ipAddress=='::1') && (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN')){
+                                $comando = 'ipconfig /all';
+                                ob_start();
+                                system($comando);
+                                $mycom=ob_get_contents();
+                                ob_clean();
+                                $findme = "sica";
+                                $pmac = strpos($mycom, $findme);
+                                $mac=substr($mycom,($pmac+32),17);
+                        }
+                        else{
+                                $mac=false;
+                                if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+                                        $comando = 'arp -a ';
+                                        ob_start();
+                                        $arp=system($comando.$ipAddress);
+                                        ob_clean();
+                                        $lines=explode("\n", $arp);
+                                        foreach($lines as $line)
+                                        {
+                                           $cols=preg_split('/\s+/', trim($line));
+                                           if ($cols[0]==$ipAddress)
+                                           {
+                                                   $mac=$cols[1];
+                                           }
+                                        }
+                                } else {
+                                        $comando = "sudo arp -a | grep $ipAddress | awk '{print $4}'";
+                                        ob_start();
+                                        $mac=exec($comando);
+                                        ob_clean();
+                                }
+                        }
+                        return str_replace(":","-",utf8_decode($mac));
+                        //return $mac;
+     }
+?>
